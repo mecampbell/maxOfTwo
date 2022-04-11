7e17fc921b128ff42731fb63728347c1cdb221ee
Michael Campbell <michaelmichael323@hotmail.com>
2022-04-11 11:42:11 -0600

    Initial Commit

---
+++ maxOfTwo.js
@@ -0,0 +1,10 @@
+let x = 1
+let y = 2
+
+// Write your code below this line.
+
+// Write a block of code that will print out the largest value.
+// Using the given values as an example, your code should print out 2.
+// To verify your code is working properly, try and change the values of x and y.
+// As an additional challenge, print out The values are identical. if x and y have the same value.
+
